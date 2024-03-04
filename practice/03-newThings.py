498b200748c0446515d6355ace8a04769f8f6cbf
Shrut27Makadiya <109706539+Shrutmakadiya@users.noreply.github.com>
2024-03-04 16:47:30 +0000

    add some unique

---
+++ practice/03-newThings.py
@@ -0,0 +1,12 @@
+name = "shrut"
+sem = "6th"
+print("h" in name)
+
+s1 = "my name is {} and i study in {} sem"
+print(s1.format(name,sem))
+print(s1.format(sem,name))
+
+# path = "c:\user\word"
+# path = r"c:\user\word"
+# path = "c:\\user\\word"
+# print(path)
